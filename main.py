4982e644fac7e1d632266a3c590a39a12dd99818
hankhank10 <m.a.hankinson@gmail.com>
2022-12-01 21:52:08 +0000

    Test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+import random
 
 app = Flask(__name__)
 
@@ -11,10 +12,15 @@ def index():
 @app.route('/submit', methods=['POST'])
 def api():
 
+    # For demonstration purposes we will have the server return a simulated failure 70% of the time
+    pct_chance_of_simulated_failure = 70
+    if random.randint(0, 100) < pct_chance_of_simulated_failure:
+        return jsonify({'success': False, 'message': 'Simulated failure'}), 500
+
     # Try to get the data from the request, and if successful print it to the console and return a success message
     try:
         data = request.get_json()
-        print("Array received: ", data)
+        print("Array received successfully: ", data)
         return jsonify(data), 200
 
     # If the request fails, print the error to the console and return an error message
